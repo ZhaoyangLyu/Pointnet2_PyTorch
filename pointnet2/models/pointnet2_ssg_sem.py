e8980611e429fb00ebcfe954380e3db0dba788f5
ZhaoyangLyu <1155113942@link.cuhk.edu.hk>
2021-03-08 11:01:18 +0800

    test segmentation mode

--- pointnet2/models/pointnet2_ssg_sem.py
+++ pointnet2/models/pointnet2_ssg_sem.py
@@ -11,12 +11,13 @@ from pointnet2.models.pointnet2_ssg_cls import PointNet2ClassificationSSG
 class PointNet2SemSegSSG(PointNet2ClassificationSSG):
     def _build_model(self):
         self.SA_modules = nn.ModuleList()
+        # pdb.set_trace()
         self.SA_modules.append(
             PointnetSAModule(
                 npoint=1024,
                 radius=0.1,
                 nsample=32,
-                mlp=[6, 32, 32, 64],
+                mlp=[self.hparams['in_fea_dim'], 32, 32, 64],
                 use_xyz=self.hparams["model.use_xyz"],
             )
         )
@@ -49,7 +50,8 @@ class PointNet2SemSegSSG(PointNet2ClassificationSSG):
         )
 
         self.FP_modules = nn.ModuleList()
-        self.FP_modules.append(PointnetFPModule(mlp=[128 + 6, 128, 128, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[128 + self.hparams['in_fea_dim'], 
+                                                128, 128, 128]))
         self.FP_modules.append(PointnetFPModule(mlp=[256 + 64, 256, 128]))
         self.FP_modules.append(PointnetFPModule(mlp=[256 + 128, 256, 256]))
         self.FP_modules.append(PointnetFPModule(mlp=[512 + 256, 256, 256]))
@@ -59,7 +61,7 @@ class PointNet2SemSegSSG(PointNet2ClassificationSSG):
             nn.BatchNorm1d(128),
             nn.ReLU(True),
             nn.Dropout(0.5),
-            nn.Conv1d(128, 13, kernel_size=1),
+            nn.Conv1d(128, self.hparams['out_dim'], kernel_size=1),
         )
 
     def forward(self, pointcloud):
@@ -92,3 +94,22 @@ class PointNet2SemSegSSG(PointNet2ClassificationSSG):
     def prepare_data(self):
         self.train_dset = Indoor3DSemSeg(self.hparams["num_points"], train=True)
         self.val_dset = Indoor3DSemSeg(self.hparams["num_points"], train=False)
+
+import pdb
+if __name__ == '__main__':
+    print(0)
+    param = {}
+    param["model.use_xyz"] = True
+    param['in_fea_dim'] = 0
+    param['out_dim'] = 3
+    device = torch.device('cuda:0')
+    pnet_sem = PointNet2SemSegSSG(param)
+    pnet_sem.to(device)
+
+    B = 32
+    N = 1024
+    
+    cloud = torch.rand(B,N, param['in_fea_dim']+3).to(device)
+    pdb.set_trace()
+    out = pnet_sem(cloud)
+    pdb.set_trace()
